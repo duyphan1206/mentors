c533f1c0f78ba9cdb61745dc2b42a9164b13b5ac
vagrant <vagrant@precise32.(none)>
2014-01-15 10:08:24 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mentors::Application.config.secret_key_base = '3594f02551d10f818c9072c84ae8d8fd66d0d994f4ddbaf9dbca6ee8e05c6873440e26558aae177fd023e038134065dc29327ce0bf2c6bfef1437c6dbf674627'
